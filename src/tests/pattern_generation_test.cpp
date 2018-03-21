96ad95b4daa2e5d615c71c127a34d05e805cf49e
rui <ruihortafigueiredo@gmail.com>
2018-03-21 08:36:08 +0000

    added the possibility of generating textures separately

--- src/tests/pattern_generation_test.cpp
+++ src/tests/pattern_generation_test.cpp
@@ -73,56 +73,13 @@
 /// Default dataset directory
 #define ARG_DATASET_DIR_DEFAULT "dataset"
 
-//////////////////////////////////////////////////
-const std::string getUsage(const char* argv_0)
-{
-    return \
-        "usage:   " + std::string(argv_0) + " [options]\n" +
-        "options: -n <number of scenes to generate>\n"  +
-        "         -s <index of the first scene>\n" +
-        "         -d <dataset output directory>\n" +
-        "         -r <images resolution>\n";
-}
+#define UNIFORM  "uniform"
+#define PERLIN   "perlin"
+#define GRADIENT "gradient"
+#define ALL      "all"
 
 
-void parseArgs(
-    int argc,
-    char** argv,
-    unsigned int & scenes,
-    unsigned int & start,
-    std::string & dataset,
-    unsigned int & resolution)
-{
-
-    int opt;
-    bool d, s, r;
-
-    while ((opt = getopt(argc,argv,"n: d: t: s: r:")) != EOF)
-    {
-        switch (opt)
-        {
-            case 'n':
-                scenes = atoi(optarg); break;
-            case 'd':
-                d = true; dataset = optarg;    break;
-            case 't':
-                std::cerr << getUsage(argv[0]);
-            case 's':
-                s = true; s = optarg;    break;
-            case 'r':
-                r = true; resolution = atoi(optarg);    break;
-            default:
-                std::cout << std::endl;
-                exit(EXIT_FAILURE);
-        }
-    }
-
-    // If arg was not set then assign default values
-    if (!s) scenes      = ARG_SCENES_DEFAULT;
-    if (!d) dataset    = ARG_DATASET_DIR_DEFAULT;
-    if (!r) resolution    = ARG_IMG_RESOLUTION_DEFAULT;
-}
-
+#define ARG_TYPE_DEFAULT "dataset"
 
 
 void genScript(const std::string material_name, const std::string & SCRIPTS_DIR){
@@ -158,12 +115,167 @@ void genScript(const std::string material_name, const std::string & SCRIPTS_DIR)
     }
 }
 
+
+//////////////////////////////////////////////////
+const std::string getUsage(const char* argv_0)
+{
+    return \
+        "usage:   " + std::string(argv_0) + " [options]\n" +
+        "options: -n <number of scenes to generate>\n"  +
+        "         -i <index of the first scene>\n" +
+        "         -d <dataset output directory>\n" +
+        "         -t <texture type>\n" +
+        "         -r <images resolution>\n";
+}
+
+cv::Mat generateFlatTexture(PatternGeneration & pattern_generation, unsigned int & resolution, const unsigned int & i, std::string & TEXTURES_DIR, std::string & SCRIPTS_DIR)
+{
+	cv::Scalar flat_color       = pattern_generation.getRandomColor();
+	/* Generate flat texture */
+
+	cv::Mat flat_texture = pattern_generation.getFlatTexture(flat_color,resolution);
+       
+        std::stringstream material_name;
+        std::stringstream img_filename;
+
+	material_name.str(std::string());
+	img_filename.str(std::string());
+
+	material_name << "flat_" << std::to_string(i);
+	img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
+
+	if (!cv::imwrite(img_filename.str(), flat_texture)){
+	    std::cout << "[ERROR] Could not save " << img_filename.str() <<
+	    ". Please ensure the destination folder exists!" << std::endl;
+	    exit(EXIT_FAILURE);
+	}
+	genScript(material_name.str(), SCRIPTS_DIR);
+
+	if (SHOW_IMGS)
+	    cv::imshow("Flat texture", flat_texture);
+}
+
+cv::Mat generateSquareTexture(PatternGeneration & pattern_generation, unsigned int & resolution, const unsigned int & i, std::string & TEXTURES_DIR, std::string & SCRIPTS_DIR)
+{
+	/* Initialize random device */
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::uniform_int_distribution<int> dist;
+	int squares;
+	int block_size;
+	/* Generate square texture */
+	//generateSquareTexture(pattern_generation, resolution, i, TEXTURES_DIR, SCRIPTS_DIR);
+	squares = dist(mt) % 20 + 8;     // v2 in the range 1 to 100
+	block_size = resolution / squares;
+	if (block_size % 2 == 0)
+	    block_size++;
+
+
+	cv::Scalar chess_color_1    = pattern_generation.getRandomColor();
+	cv::Scalar chess_color_2    = pattern_generation.getRandomColor();
+
+	cv::Mat chess_board = pattern_generation.getChessTexture(chess_color_1, chess_color_2, block_size, squares);
+
+	// Convert to HSV
+	cv::applyColorMap(chess_board, chess_board, cv::COLORMAP_HSV);
+
+        std::stringstream material_name;
+        std::stringstream img_filename;
+
+	/* Reset string stream values */
+	material_name.str(std::string());
+	img_filename.str(std::string());
+
+	material_name << "chess_" << std::to_string(i);
+	img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
+
+	if (!cv::imwrite(img_filename.str(), chess_board)){
+	    std::cout << "[ERROR] Could not save " << img_filename.str() <<
+	    ". Please ensure the destination folder exists!" << std::endl;
+	    exit(EXIT_FAILURE);
+	}
+	genScript(material_name.str(), SCRIPTS_DIR);
+
+	if (SHOW_IMGS)
+	    cv::imshow("Chess board", chess_board);
+}
+
+
+cv::Mat generateGradientTexture(PatternGeneration & pattern_generation, unsigned int & resolution, const unsigned int & i, std::string & TEXTURES_DIR, std::string & SCRIPTS_DIR)
+{
+        std::stringstream material_name;
+        std::stringstream img_filename;
+	cv::Scalar gradient_color_1 = pattern_generation.getRandomColor();
+	cv::Scalar gradient_color_2 = pattern_generation.getRandomColor();
+	cv::Mat gradient_texture = pattern_generation.getGradientTexture(gradient_color_1, gradient_color_2, resolution, false);
+
+	material_name.str(std::string());
+	img_filename.str(std::string());
+
+	material_name << "gradient_" << std::to_string(i);
+	img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
+
+	if (!cv::imwrite(img_filename.str(), gradient_texture)){
+	std::cout << "[ERROR] Could not save " << img_filename.str() <<
+	    ". Please ensure the destination folder exists!" << std::endl;
+	    exit(EXIT_FAILURE);
+	}
+	genScript(material_name.str(), SCRIPTS_DIR);
+
+	if (SHOW_IMGS)
+	    cv::imshow("Gradient texture", gradient_texture);
+}
+
+void parseArgs(
+    int argc,
+    char** argv,
+    unsigned int & scenes,
+    unsigned int & start,
+    std::string & dataset,
+    unsigned int & resolution,
+    std::string & type)
+{
+
+    int opt;
+    bool d, t, n, i, s, r;
+
+    while ( (opt = getopt(argc,argv,"n: d: t: s: r: h: i:")) != EOF)
+    {
+        switch (opt)
+        {
+            case 'd':
+                d=true; dataset = optarg;    break;
+            case 't':
+                t=true; type = atoi(optarg); break;
+            case 'n':
+                s=true; scenes = atoi(optarg); break;
+            case 'i':
+                i=true; start = atoi(optarg); break;
+            case 'r':
+                r=true; resolution = atoi(optarg);    break;
+            default:
+                std::cout << std::endl;
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    // If arg was not set then assign default values
+    if (!s) scenes      = ARG_SCENES_DEFAULT;
+    if (!d) dataset     = ARG_DATASET_DIR_DEFAULT;
+    if (!r) resolution  = ARG_IMG_RESOLUTION_DEFAULT;
+    if (!i) start       = ARG_START_DEFAULT;
+    if (!t) type        = ARG_TYPE_DEFAULT;
+}
+
+
+
 int main(int argc, char **argv)
 {
     // Command-line arguments
     unsigned int scenes {0};
     unsigned int start {0};
     unsigned int resolution {0};
+    std::string type;
     std::string media_dir;
     std::string dataset_dir;
 
@@ -172,7 +284,7 @@ int main(int argc, char **argv)
 
     /* root directory */
 
-    parseArgs(argc, argv, scenes, start, media_dir, resolution);
+    parseArgs(argc, argv, scenes, start, media_dir, resolution, type);
     std::string TEXTURES_DIR=media_dir+"textures/";
     std::string SCRIPTS_DIR=media_dir+"scripts/";
 
@@ -194,129 +306,48 @@ int main(int argc, char **argv)
     /* Pattern generator object instance */   
     PatternGeneration pattern_generation;
 
-    int img_size = 500;
-    int squares;
-    int block_size;
+
 
     std::stringstream material_name;
     std::stringstream img_filename;
-
-    for (unsigned int i = 0; i < scenes; ++i)
+    if(type=="all")
     {
-        /* Generate square texture */
-
-        squares = dist(mt) % 20 + 8;     // v2 in the range 1 to 100
-        block_size = img_size / squares;
-        if (block_size % 2 == 0)
-            block_size++;
-
-        /*if(HSV==domain_)
-        {
-            cv::applyColorMap(color, HSV, CV_HSV2RGB);
-
-        }
-        else if(HSL==domain_){
-            cv::applyColorMap(color, HSV, CV_HLS2RGB);
-
-        }*/
-
-        cv::Scalar chess_color_1 = pattern_generation.getRandomColor();
-        cv::Scalar chess_color_2 = pattern_generation.getRandomColor();
-        cv::Scalar flat_color = pattern_generation.getRandomColor();
-        cv::Scalar gradient_color_1 = pattern_generation.getRandomColor();
-        cv::Scalar gradient_color_2 = pattern_generation.getRandomColor();
-
-        cv::Mat chess_board = pattern_generation.getChessTexture(chess_color_1, chess_color_2, block_size, squares);
-
-        // Convert to HSV
-        cv::applyColorMap(chess_board, chess_board, cv::COLORMAP_HSV);
+	    for (unsigned int i = start; i < scenes; ++i)
+	    {
+                /* Generate flat texture */
+		generateFlatTexture(pattern_generation, resolution, i, TEXTURES_DIR, SCRIPTS_DIR);
 
 
-        /* Reset string stream values */
-        material_name.str(std::string());
-        img_filename.str(std::string());
+		/* Generate gradient texture */
+		generateGradientTexture(pattern_generation, resolution, i, TEXTURES_DIR, SCRIPTS_DIR);
 
-        material_name << "chess_" << std::to_string(i);
-        img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
+		/* Generate perlin noise texture */
+		double z1=((double) dist(mt) / (RAND_MAX));
+		double z2=((double) dist(mt) / (RAND_MAX));
+		double z3=((double) dist(mt) / (RAND_MAX));
+		srand(time(0));
+		int randomval = dist(mt) % 2;
 
-        if (!cv::imwrite(img_filename.str(), chess_board)){
-            std::cout << "[ERROR] Could not save " << img_filename.str() <<
-            ". Please ensure the destination folder exists!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        genScript(material_name.str(), SCRIPTS_DIR);
+		cv::Mat perlin_texture = pattern_generation.getPerlinNoiseTexture(resolution,randomval,z1,z2,z3);
 
-        if (SHOW_IMGS)
-            cv::imshow("Chess board", chess_board);
+		material_name.str(std::string());
+		img_filename.str(std::string());
 
+		material_name << "perlin_" << std::to_string(i);
+		img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
 
-        /* Generate flat texture */
+		if (!cv::imwrite(img_filename.str(), perlin_texture)){
+		std::cout << "[ERROR] Could not save " << img_filename.str() <<
+		    ". Please ensure the destination folder exists!" << std::endl;
+		    exit(EXIT_FAILURE);
+		}
+		genScript(material_name.str(), SCRIPTS_DIR);
 
-        cv::Mat flat_texture = pattern_generation.getFlatTexture(flat_color,img_size);
-
-        material_name.str(std::string());
-        img_filename.str(std::string());
-
-        material_name << "flat_" << std::to_string(i);
-        img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
-
-        if (!cv::imwrite(img_filename.str(), flat_texture)){
-            std::cout << "[ERROR] Could not save " << img_filename.str() <<
-            ". Please ensure the destination folder exists!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        genScript(material_name.str(), SCRIPTS_DIR);
-
-        if (SHOW_IMGS)
-            cv::imshow("Flat texture", flat_texture);
-
-        /* Generate gradient texture */
-
-        cv::Mat gradient_texture = pattern_generation.getGradientTexture(gradient_color_1, gradient_color_2, img_size, false);
-
-        material_name.str(std::string());
-        img_filename.str(std::string());
-
-        material_name << "gradient_" << std::to_string(i);
-        img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
-
-        if (!cv::imwrite(img_filename.str(), gradient_texture)){
-        std::cout << "[ERROR] Could not save " << img_filename.str() <<
-            ". Please ensure the destination folder exists!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        genScript(material_name.str(), SCRIPTS_DIR);
-
-        if (SHOW_IMGS)
-            cv::imshow("Gradient texture", gradient_texture);
-
-        /* Generate perlin noise texture */
-        double z1=((double) dist(mt) / (RAND_MAX));
-        double z2=((double) dist(mt) / (RAND_MAX));
-        double z3=((double) dist(mt) / (RAND_MAX));
-        srand(time(0));
-        int randomval = dist(mt) % 2;
-
-        cv::Mat perlin_texture = pattern_generation.getPerlinNoiseTexture(img_size,randomval,z1,z2,z3);
-
-        material_name.str(std::string());
-        img_filename.str(std::string());
-
-        material_name << "perlin_" << std::to_string(i);
-        img_filename << TEXTURES_DIR << material_name.str() << ".jpg";
-
-        if (!cv::imwrite(img_filename.str(), perlin_texture)){
-        std::cout << "[ERROR] Could not save " << img_filename.str() <<
-            ". Please ensure the destination folder exists!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        genScript(material_name.str(), SCRIPTS_DIR);
-
-        if (SHOW_IMGS)
-            cv::imshow("Perlin noise texture", perlin_texture);
-
-
+		if (SHOW_IMGS)
+		    cv::imshow("Perlin noise texture", perlin_texture);
+	    }
     }
+    
 
     return 0;
 }
